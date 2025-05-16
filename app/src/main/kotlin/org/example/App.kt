97f84286b8c31e21ac2828aca41da10890bd0fa4
emirtaboada <e.taboada@alumno.etec.um.edu.ar>
2025-05-16 10:33:18 -0300

    Etapa 4: Implementación de funciones avanzadas

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -75,18 +75,26 @@ fun calcularPromedioTresNotas(nota1: Double, nota2: Double, nota3: Double): Doub
 
 fun obtenerEstadoAlumno(nombre: String, apellido: String, nota: Double): String {
     // Implementar aquí
-    return ""
+    val estado = if (nota >= 6) "está aprobado" else "está desaprobado"
+    return "El alumno $nombre $apellido $estado"
 }
 
 // Etapa 3
 fun calcularPromedioCurso(notas: List<Double>): Double {
     // Implementar aquí
-    return 0.0
+    if (notas.isEmpty()) return 0.0
+    return notas.sum() / notas.size
 }
 
 fun obtenerAlumnosAprobados(nombres: List<String>, notas: List<Double>): List<String> {
     // Implementar aquí
-    return emptyList()
+    val aprobados = mutableListOf<String>()
+    for (i in nombres.indices) {
+        if (notas[i] >= 6.0) {
+            aprobados.add(nombres[i])
+        }
+    }
+    return aprobados
 }
 
 // Etapa 4
